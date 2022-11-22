2180e6c8c9f58ca10ad1d3f6e300734a2023e182
arpitkarnak <arpitkarnak@users.noreply.github.com>
2022-11-22 15:43:23 +0530

    added button

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+var buttonOne = document.querySelector('#button1')
+var buttonTwo = document.querySelector('#button2')
+var input1 = document.querySelector('#input')
+var output1 = document.querySelector('#output')
+
+var currentSize = 15;
+
+
