2ea6df8b090af407ca2bec7ec4a10f37249798a9
arpitkarnak <arpitkarnak@users.noreply.github.com>
2022-11-22 21:23:19 +0530

    increase & descrease font size on click

--- app.js
+++ app.js
@@ -5,6 +5,40 @@ var buttonTwo = document.querySelector('#button2')
 var input1 = document.querySelector('#input')
 var output1 = document.querySelector('#output')
 
-var currentSize = 15;
+
+var currentSize= 12;
+
+
+function increaseFont(){
+    
+   
+    currentSize = currentSize + 2;
+    // console.log(currentSize);
+    output1.style.fontSize=currentSize+"px";
+    output1.innerText = input1.value;
+
+}
+
+
+function descreaseFont(){
+
+    currentSize = currentSize-2;
+    output1.style.fontSize=currentSize+"px"
+
+    output1.innerText = input1.value;
+}
+
+
+
+
+
+
+
+
+buttonOne.addEventListener('click',increaseFont)
+
+buttonTwo.addEventListener('click',descreaseFont)
+
+
 
 
